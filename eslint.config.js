cffbe1ec03d0bb6c33e1650021fd941da6769f74
Geeks_U <cncdre11@outlook.com>
2025-04-20 16:27:26 +0800

    项目初始化_ESlint基础配置完成

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  // 全局配置
+  {
+    files: ['**/*.{js,cjs,mjs,ts,vue}'],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.browser },
+    },
+    plugins: { js },
+    extends: [js.configs.recommended],
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'no-unused-vars': 'off',
+    },
+  },
+
+  // TS配置
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    extends: [tseslint.configs.recommended],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+
+  // VUE配置
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+    },
+    extends: [pluginVue.configs['flat/essential']],
+    rules: {
+      'vue/no-unused-vars': 'warn',
+    },
+  },
+])
